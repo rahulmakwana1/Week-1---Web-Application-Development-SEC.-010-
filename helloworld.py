eaceef9e478f03758e08a76aa6c72467cad91bb7
rahul makwana <rahulmakwana3535@gmail.com>
2021-09-13 12:48:08 -0400

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,6 +0,0 @@
-Python 3.10.0rc2 (tags/v3.10.0rc2:839d789, Sep  7 2021, 18:51:45) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-print"hello world!"
-SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
-print("hello world")
-hello world
